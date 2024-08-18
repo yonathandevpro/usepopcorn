99c5293119a995bd56de63cb98ae35a970f1dbb4
yonathandevpro <yonathanber@icloud.com>
2024-08-18 16:12:20 +0300

    incorporated API calls

--- src/App-v1.jsx
+++ src/App-v1.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,25 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "b8475ce";
-
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-  const query = "Interstellar";
-
-  useEffect(function () {
-    async function fetchMovies() {
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-      );
-
-      const data = await res.json();
-      setMovies(data.Search);
-    }
-    fetchMovies();
-  }, []);
-
   return (
     <>
       <NavBar>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
 import StarRating from "./StarRating";
 
@@ -17,12 +17,12 @@ function Test() {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
